a06bb8283bc89308ce4245819c6ca8743e6ba282
Matt Kramer <matt314159@gmail.com>
2023-02-16 14:13:38 -0800

    Add load_yaml.py

---
+++ scripts/load_yaml.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from fireworks.core.launchpad import LaunchPad
+import ruamel.yaml as yaml
+
+COLLECTIONS = ['repos', 'runners', 'base_envs']
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('infile')
+    ap.add_argument('--db', default='arcubeprod')
+    args = ap.parse_args()
+
+    data = yaml.safe_load(open(args.infile))
+
+    lpad = LaunchPad.auto_load()
+    db = lpad.connection[args.db]
+
+    for collection, docs in data.items():
+        assert collection in COLLECTIONS
+        c = db[collection]      # auto creates
+        c.insert_many(docs)
+
+
+if __name__ == '__main__':
+    main()
