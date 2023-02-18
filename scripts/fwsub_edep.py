2cc28ae9fb2b8f6aea3ca0936b8c0cea52280097
Matt Kramer <matt314159@gmail.com>
2023-02-17 21:58:14 -0800

    Update fwsub_edep.py

--- scripts/fwsub_edep.py
+++ scripts/fwsub_edep.py
@@ -10,32 +10,43 @@ from fw4dune_tasks import RepoRunner
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--runner', default='SimFor2x2_v1_GenieEdep')
-    ap.add_argument('--base-env', default='SimFor2x2_v1_MiniRun1_1E19_RHC_rock_tiny')
-    ap.add_argument('--name')
-    # ap.add_argument('--size', default=5120)
-    ap.add_argument('--size', default=64)
+    ap.add_argument('--runner', required=True)
+    ap.add_argument('--base-env', required=True)
+    ap.add_argument('--name', help='Defaults to --base-env; controls output directory')
+    ap.add_argument('--worker', help='Defaults to --name; controls which workers can produce these files')
+    ap.add_argument('--size', type=int, default=1, help='Number of files to produce')
     args = ap.parse_args()
 
+    if args.name is None:
+        args.name = args.base_env
+
+    if args.worker is None:
+        args.worker = args.name
+
     lpad = LaunchPad.auto_load()
+    db = lpad.connection[lpad.name]
+
+    if not db['runners'].find_one({'name': args.runner}):
+        print(f'PANIK: Runner {args.runner} does not exist')
+        raise
+
+    if not db['base_envs'].find_one({'name': args.base_env}):
+        print(f'PANIK: Base-env {args.base_env} does not exist')
+        raise
 
     for index in range(args.size):
         spec = {
             'runner': args.runner,
             'base_env': args.base_env,
             'env': {
+                'ARCUBE_OUT_NAME': args.name,
                 'ARCUBE_INDEX': str(index),
             },
-            '_fworker': 'fworker_edep_sim'
+            '_fworker': args.worker,
+            '_category': args.worker,
         }
 
-        if args.name:
-            spec['env']['ARCUBE_OUT_NAME'] = name
-        # otherwise use ARCUBE_OUT_NAME from the base_env
-
-        fw_name = args.name if args.name else args.base_env
-
-        fw = Firework(RepoRunner(), name=fw_name, spec=spec)
+        fw = Firework(RepoRunner(), name=args.name, spec=spec)
         lpad.add_wf(fw)
 
 
