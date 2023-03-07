b1bf543f2698adf28f065f00cc7d6157640f1e7e
Matt Kramer <matt314159@gmail.com>
2023-03-06 19:41:32 -0800

    Give fwsub.py more reasonable defaults
    Goal: If a base-env yaml specifies ARCUBE_OUT_NAME, respect it. Don't
    force us to specify --name lest $base_env be made to override the yaml's
    ARCUBE_OUT_NAME. If that makes sense.

--- scripts/fwsub.py
+++ scripts/fwsub.py
@@ -12,41 +12,44 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--runner', required=True)
     ap.add_argument('--base-env', required=True)
-    ap.add_argument('--name', help='Defaults to --base-env; controls output directory')
-    ap.add_argument('--worker', help='Defaults to --name; controls which workers can produce these files')
+    ap.add_argument('--name', help='Output dir; defaults to base_env.ARCUBE_OUT_NAME if available; otherwise --base-env')
+    ap.add_argument('--worker', help='Controls which workers can produce these files; defaults to --base-env')
     ap.add_argument('--size', type=int, default=1, help='Number of files to produce')
+    ap.add_argument('--start', type=int, default=0, help='Starting index of output files')
     args = ap.parse_args()
 
-    if args.name is None:
-        args.name = args.base_env
-
     if args.worker is None:
-        args.worker = args.name
+        args.worker = args.base_env
 
     lpad = LaunchPad.auto_load()
     db = lpad.connection[lpad.name]
 
-    if not db['runners'].find_one({'name': args.runner}):
-        print(f'PANIK: Runner {args.runner} does not exist')
-        raise
+    assert db['runners'].find_one({'name': args.runner}), \
+        f'Runner "{args.runner}" must exist'
 
-    if not db['base_envs'].find_one({'name': args.base_env}):
-        print(f'PANIK: Base-env {args.base_env} does not exist')
-        raise
+    base_env_dict = db['base_envs'].find_one({'name': args.base_env})
+    assert base_env_dict, \
+        f'Base-env "{args.base_env}" must exist'
 
-    for index in range(args.size):
+    for index in range(args.start, args.size):
         spec = {
             'runner': args.runner,
             'base_env': args.base_env,
             'env': {
-                'ARCUBE_OUT_NAME': args.name,
                 'ARCUBE_INDEX': str(index),
             },
             '_fworker': args.worker,
             '_category': args.worker,
         }
 
-        fw = Firework(RepoRunner(), name=args.name, spec=spec)
+        if args.name:
+            spec['env']['ARCUBE_OUT_NAME'] = name
+
+        elif 'ARCUBE_OUT_NAME' not in base_env_dict['env']:
+            spec['env']['ARCUBE_OUT_NAME'] = args.base_env
+
+        fw_name = args.name if args.name else args.base_env
+        fw = Firework(RepoRunner(), name=fw_name, spec=spec)
         lpad.add_wf(fw)
 
 
