7fd8f36b880eab5691e488016d191319eeb1117b
Matt Kramer <matt314159@gmail.com>
2023-03-07 17:48:09 -0800

    Change behavior of fwsub.py w.r.t. "name" etc
    Still iterating on this

--- scripts/fwsub.py
+++ scripts/fwsub.py
@@ -13,13 +13,10 @@ def main():
     ap.add_argument('--runner', required=True)
     ap.add_argument('--base-env', required=True)
     ap.add_argument('--name', help='Output dir; defaults to base_env.ARCUBE_OUT_NAME if available; otherwise --base-env')
-    ap.add_argument('--worker', help='Controls which workers can produce these files; defaults to --base-env')
+    ap.add_argument('--worker', help='Controls which workers can produce these files; defaults to "name"')
     ap.add_argument('--size', type=int, default=1, help='Number of files to produce')
     ap.add_argument('--start', type=int, default=0, help='Starting index of output files')
     args = ap.parse_args()
-
-    if args.worker is None:
-        args.worker = args.base_env
 
     lpad = LaunchPad.auto_load()
     db = lpad.connection[lpad.name]
@@ -31,25 +28,30 @@ def main():
     assert base_env_dict, \
         f'Base-env "{args.base_env}" must exist'
 
+    if args.name:
+        name = args.name
+    # NOTE: We should probably avoid putting ARCUBE_OUT_NAME in the base_env
+    elif 'ARCUBE_OUT_NAME' in base_env_dict['env']:
+        name = base_env_dict['env']['ARCUBE_OUT_NAME']
+    else:
+        name = args.base_env
+
+    if args.worker is None:
+        args.worker = name
+
     for index in range(args.start, args.start + args.size):
         spec = {
             'runner': args.runner,
             'base_env': args.base_env,
             'env': {
+                'ARCUBE_OUT_NAME': name,
                 'ARCUBE_INDEX': str(index),
             },
             '_fworker': args.worker,
             '_category': args.worker,
         }
 
-        if args.name:
-            spec['env']['ARCUBE_OUT_NAME'] = args.name
-
-        elif 'ARCUBE_OUT_NAME' not in base_env_dict['env']:
-            spec['env']['ARCUBE_OUT_NAME'] = args.base_env
-
-        fw_name = args.name if args.name else args.base_env
-        fw = Firework(RepoRunner(), name=fw_name, spec=spec)
+        fw = Firework(RepoRunner(), name=name, spec=spec)
         lpad.add_wf(fw)
 
 
