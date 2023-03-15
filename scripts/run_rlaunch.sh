21c4bb77a0b58348e400ae59a92bb13ef49c9508
Matt Kramer <matt314159@gmail.com>
2023-03-15 15:33:23 -0700

    Add run_rlaunch.sh

---
+++ scripts/run_rlaunch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo "Usage: $0 fworker_name rlaunch_mode rlaunch_args..."
+    echo "fworker_name corresponds to _fworker in the DB"
+    echo "rlaunch_mode should be singleshot or rapidfire"
+    exit 1
+}
+
+fworker_name=$1; shift
+if [[ -z "$fworker_name" ]]; then
+    usage
+fi
+
+rlaunch_mode=$1; shift
+if [[ "$rlaunch_mode" != "singleshot" &&
+          "$rlaunch_mode" != "rapidfire" ]]; then
+    usage
+fi
+
+rlaunch_args=("$@"); shift $#
+
+fworker_yaml=$(mktemp --suffix=.yaml)
+cat > "$fworker_yaml" <<EOF
+name: '$fworker_name'
+category: '$fworker_name'
+query: '{}'
+EOF
+
+rlaunch -w "$fworker_yaml" "${rlaunch_args[@]}" "$rlaunch_mode"
+
+rm $fworker_yaml
