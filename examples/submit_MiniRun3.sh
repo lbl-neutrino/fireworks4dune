d5d808e0e466e885133bf4981f764f2ed7e84f18
Matt Kramer <mjkramer@users.noreply.github.com>
2023-04-14 20:14:09 -0700

    Add MiniRun3 validation plots

--- examples/submit_MiniRun3.sh
+++ examples/submit_MiniRun3.sh
@@ -16,6 +16,7 @@ scripts/fwsub.py --runner SimFor2x2_v3_SpillBuild --base-env MiniRun3_1E19_RHC.s
 scripts/fwsub.py --runner SimFor2x2_v3_Convert2H5 --base-env MiniRun3_1E19_RHC.convert2h5 --size $spill_size --start $start
 scripts/fwsub.py --runner SimFor2x2_v3_LArND --base-env MiniRun3_1E19_RHC.larnd --size $spill_size --start $start
 scripts/fwsub.py --runner SimFor2x2_v3_Flow --base-env MiniRun3_1E19_RHC.flow --size $spill_size --start $start
+scripts/fwsub.py --runner SimFor2x2_v3_Plots --base-env MiniRun3_1E19_RHC.plots --size $spill_size --start $start
 
 logdir=$SCRATCH/job_logs.MiniRun3_1E19_RHC
 mkdir -p $logdir
@@ -35,3 +36,5 @@ sbatch -o $logdir/slurm-%j.txt --array=1-1 -N 1 slurm/MiniRun3/MiniRun3_1E19_RHC
 sbatch -o $logdir/slurm-%j.txt --array=1-12 -N 4 slurm/MiniRun3/MiniRun3_1E19_RHC.larnd.slurm.sh
 
 sbatch -o $logdir/slurm-%j.txt --array=1-1 -N 1 slurm/MiniRun3/MiniRun3_1E19_RHC.flow.slurm.sh
+
+sbatch -o $logdir/slurm-%j.txt --array=1-1 -N 4 slurm/MiniRun3/MiniRun3_1E19_RHC.plots.slurm.sh
