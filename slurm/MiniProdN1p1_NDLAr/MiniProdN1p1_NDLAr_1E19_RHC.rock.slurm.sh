3fdab452a1e34332287a37856ec5655569ee1caf
Alexander Booth <abooth@login32.chn>
2023-06-02 03:16:27 -0700

    slurm dir for MiniProdN1p1_NDLAr.

---
+++ slurm/MiniProdN1p1_NDLAr/MiniProdN1p1_NDLAr_1E19_RHC.rock.slurm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --account=dune
+#SBATCH --qos=regular
+#SBATCH --constraint=cpu
+#SBATCH --time=4:00:00
+#SBATCH --ntasks-per-node=256
+
+name=MiniProdN1p1_NDLAr_1E19_RHC.rock
+
+basedir=$PWD
+
+logdir=${SCRATCH}/logs.${name}/${SLURM_JOBID}
+mkdir -p "$logdir"
+
+launchdir=${SCRATCH}/launchers.${name}
+# launchdir=launchers.${name}
+mkdir -p "$launchdir"
+cd "$launchdir" || exit
+
+srun -o "$logdir"/output-%j.%t.txt \
+    "$basedir"/scripts/run_rlaunch.sh "$name" singleshot
