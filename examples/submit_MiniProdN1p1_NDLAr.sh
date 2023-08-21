545e52f51bb1ba8f2eb700f301fdd483be0997ac
Alexander Booth <abooth@login21.chn>
2023-08-21 02:30:20 -0700

    Shuffle around submit script to be a more logical order.

--- examples/submit_MiniProdN1p1_NDLAr.sh
+++ examples/submit_MiniProdN1p1_NDLAr.sh
@@ -1,33 +1,48 @@
 #!/usr/bin/env bash
 
-source admin/load_fireworks.sh
+#source admin/load_fireworks.sh
 
-scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.yaml
+scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.nu.yaml --replace
+#scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.rock.yaml --replace
+#scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.hadd.yaml --replace
+#scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.spill.yaml --replace
+#scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.convert*.yaml --replace
+#scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p1_NDLAr/*.larnd*.yaml --replace
 
 start=0
 single_size=10240
 spill_size=1024
-logdir=$SCRATCH/job_logs.MiniProdN1p1_NDLAr_1E19_RHC
+logdir=$SCRATCH/MiniProdN1p1-v1r1/job_logs.MiniProdN1p1_NDLAr_1E19_RHC_test
 
 mkdir -p $logdir
 
-scripts/fwsub.py --runner SimForNDLAr_v1_GenieEdep --base-env MiniProdN1p1_NDLAr_1E19_RHC.nu --size $single_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_GenieEdep --base-env MiniProdN1p1_NDLAr_1E19_RHC.rock --size $single_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_Hadd --base-env MiniProdN1p1_NDLAr_1E19_RHC.nu.hadd --size $spill_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_Hadd --base-env MiniProdN1p1_NDLAr_1E19_RHC.rock.hadd --size $spill_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_SpillBuild --base-env MiniProdN1p1_NDLAr_1E19_RHC.spill --size $spill_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_Convert2H5 --base-env MiniProdN1p1_NDLAr_1E19_RHC.convert2h5 --size $spill_size --start $start
-scripts/fwsub.py --runner SimForNDLAr_v1_LArND --base-env MiniProdN1p1_NDLAr_1E19_RHC.larnd --size $spill_size --start $start
 
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-10 -N 4 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.rock rapidfire
+scripts/fwsub.py --runner SimForNDLAr_v1_GenieEdep --base-env MiniProdN1p1_NDLAr_1E19_RHC.nu --size $single_size --start $start
 sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-2 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.nu rapidfire
 
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.nu.hadd rapidfire
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.rock.hadd rapidfire
 
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 4 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.spill rapidfire
+#scripts/fwsub.py --runner SimForNDLAr_v1_GenieEdep --base-env MiniProdN1p1_NDLAr_1E19_RHC.rock --size $single_size --start $start
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-10 -N 4 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.rock rapidfire
 
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.convert2h5 rapidfire
 
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-12 -N 4 slurm/fw_gpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.larnd rapidfire --timeout $((60*40))
+#scripts/fwsub.py --runner SimForNDLAr_v1_Hadd --base-env MiniProdN1p1_NDLAr_1E19_RHC.nu.hadd --size $spill_size --start $start
+#scripts/fwsub.py --runner SimForNDLAr_v1_Hadd --base-env MiniProdN1p1_NDLAr_1E19_RHC.rock.hadd --size $spill_size --start $start
+#
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.nu.hadd rapidfire
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.rock.hadd rapidfire
+
+
+#scripts/fwsub.py --runner SimForNDLAr_v1_SpillBuild --base-env MiniProdN1p1_NDLAr_1E19_RHC.spill --size $spill_size --start $start
+#
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 4 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.spill rapidfire
+
+
+#scripts/fwsub.py --runner SimForNDLAr_v1_Convert2H5 --base-env MiniProdN1p1_NDLAr_1E19_RHC.convert2h5 --size $spill_size --start $start
+#
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.convert2h5 rapidfire
+
+
+#scripts/fwsub.py --runner SimForNDLAr_v1_LArND --base-env MiniProdN1p1_NDLAr_1E19_RHC.larnd --size $spill_size --start $start
+#
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-12 -N 4 slurm/fw_gpu.slurm.sh MiniProdN1p1_NDLAr_1E19_RHC.larnd rapidfire --timeout $((60*40))
 
