650a6b65d4c9dbab298d1bd91c3623f52c89c4d9
Matt Kramer <mjkramer@users.noreply.github.com>
2023-12-29 13:26:49 -0800

    update MR5

--- examples/submit.MiniRun5_1E19_RHC.sh
+++ examples/submit.MiniRun5_1E19_RHC.sh
@@ -14,7 +14,7 @@ scripts/load_yaml.py --replace specs/SimFor2x2_v4.yaml specs/$name/*.yaml
 
 mkdir -p "$logdir"
 
-workflows/MiniRun5_1E19_RHC.workflow.py
+workflows/submit.MiniRun5_1E19_RHC.py
 
 # lpad admin tuneup --full
 
@@ -22,3 +22,8 @@ sbatch -o "$logdir"/slurm-%j.txt --array=1-10 -N 4 -t 180 slurm/fw_cpu.slurm.sh 
 sbatch -o "$logdir"/slurm-%j.txt --array=1-2 -N 4 -t 30 slurm/fw_cpu.slurm.sh cpu_seconds rapidfire
 
 sbatch -o "$logdir"/slurm-%j.txt --array=1-1 -N 4 -t 240 slurm/fw_gpu.slurm.sh -C "gpu&hbm80g" gpu_minutes rapidfire
+
+scripts/fwsub.py --runner SimFor2x2_v4_Edep2Flat --base-env "$name.edep2flat" --size 1024 --start 0
+scripts/fwsub.py --runner SimFor2x2_v4_Minerva --base-env "$name.minerva" --size 1024 --start 0
+
+scripts/fwsub.py --runner SimFor2x2_v4_Plots --base-env "$name.plots" --name "$name.plots.test" --size 1024 --start 0
