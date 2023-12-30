e1c531ddd3ba523a941c631f8508852adfb6444f
Matt Kramer <mjkramer@users.noreply.github.com>
2023-12-29 22:41:31 -0800

    Update MegaRun5 specs

--- examples/submit.MegaRun5_1E20_RHC.sh
+++ examples/submit.MegaRun5_1E20_RHC.sh
@@ -15,11 +15,12 @@ workflows/submit.MegaRun5_1E20_RHC.py
 
 lpad admin tuneup --full
 
-sbatch -o "$logdir"/slurm-%j.txt --array=1-10 -N 4 -t 180 slurm/fw_cpu.slurm.sh mega_cpu_hours rapidfire
+sbatch -o "$logdir"/slurm-%j.txt --array=1-1 -N 4 -t 180 slurm/fw_cpu.slurm.sh mega_cpu_hours rapidfire --timeout $((60 * 140))
 sbatch -o "$logdir"/slurm-%j.txt --array=1-2 -N 4 -t 30 slurm/fw_cpu.slurm.sh mega_cpu_seconds rapidfire
 
 sbatch -o "$logdir"/slurm-%j.txt --array=1-1 -N 4 -t 240 slurm/fw_gpu.slurm.sh -C "gpu&hbm80g" mega_gpu_minutes rapidfire
 
 sbatch -o "$logdir"/slurm-%j.txt --array=1-2 -N 4 -t 30 slurm/fw_cpu.slurm.sh mega_cpu_minutes rapidfire
 
-scripts/fwsub.py --runner SimFor2x2_v4_Plots --base-env "$name.plots" --name "$name.plots.test" --size 10000 --start 0
+# Run the plots outside the Workflow
+scripts/fwsub.py --runner SimFor2x2_v4_Plots --base-env "$name.plots" --worker mega_plots --size 10000 --start 0
