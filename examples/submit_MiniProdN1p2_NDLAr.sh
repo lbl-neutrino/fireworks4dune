9024d288c67d7398f0c1eb8b7dec87086e1c596d
Alexander Booth <abooth@login09.chn.perlmutter.nersc.gov>
2024-06-20 00:57:52 -0700

    Flow specs

--- examples/submit_MiniProdN1p2_NDLAr.sh
+++ examples/submit_MiniProdN1p2_NDLAr.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #source admin/load_fireworks.sh
+set +o posix                    # sneaky sneaky, NERSC
 
 #scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p2_NDLAr/*.nu.yaml --replace
 #scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p2_NDLAr/*.rock.yaml --replace
@@ -9,13 +10,14 @@
 #scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p2_NDLAr/*.convert*.yaml --replace
 #scripts/load_yaml.py specs/SimForNDLAr_v1.yaml specs/MiniProdN1p2_NDLAr/*.larnd*.yaml --replace
 #scripts/load_yaml.py specs/ND_Production_v1.yaml specs/MiniProdN1p2_NDLAr/*.tmsreco.yaml --replace
+#scripts/load_yaml.py --replace specs/SimForNDLAr_v2.yaml specs/MiniProdN1p2_NDLAr/*.flow.yaml
 
-start=0
+start=1
 # 11008 fiducial edep-sim files after one round of draining.
 # 10650 rock edep-sim files after two rounds of draining.
-single_size=128
+single_size=11008
 # 11008 is not divisible by 10.
-spill_size=13
+spill_size=1099
 logdir=$SCRATCH/MiniProdN1p2-v1r1/job_logs.MiniProdN1p2_NDLAr_1E19_RHC
 
 mkdir -p $logdir
@@ -50,10 +52,15 @@ mkdir -p $logdir
 
 #scripts/fwsub.py --runner SimForNDLAr_v1_LArND --base-env MiniProdN1p2_NDLAr_1E19_RHC.larnd --size $spill_size --start $start
 #
-sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-2 -N 1 slurm/fw_gpu.slurm.sh MiniProdN1p2_NDLAr_1E19_RHC.larnd rapidfire
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-2 -N 1 slurm/fw_gpu.slurm.sh MiniProdN1p2_NDLAr_1E19_RHC.larnd rapidfire
 
 
 #scripts/fwsub.py --runner ND_Production_v1_TMSReco --base-env MiniProdN1p2_NDLAr_1E19_RHC.tmsreco --size $spill_size --start $start
 #
 #sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p2_NDLAr_1E19_RHC.tmsreco rapidfire
+
+
+#scripts/fwsub.py --runner SimForNDLAr_v2_Flow --base-env MiniProdN1p2_NDLAr_1E19_RHC.flow --size $spill_size --start $start
+
+sbatch -o "$logdir"/slurm-%j.txt --array=1-69 -N 1 slurm/fw_cpu.slurm.sh MiniProdN1p2_NDLAr_1E19_RHC.flow rapidfire
 
