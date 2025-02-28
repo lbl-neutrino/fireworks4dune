514d563712da31ddc24d5ea3730cd0193a5230aa
Matt Kramer <mjkramer@users.noreply.github.com>
2025-02-27 18:33:26 -0800

    Update MRF6.3 workflow script

--- workflows/fwsub.MiniRunF6.3_1E19_FHC.py
+++ workflows/fwsub.MiniRunF6.3_1E19_FHC.py
@@ -28,35 +28,34 @@ def main():
     for i in range(args.start, args.start + args.size):
         pre_hadd_range = range(i*HADD_FACTOR, (i+1)*HADD_FACTOR)
 
-        fws_genie_nu = [fwm.make(j, 'Genie', 'genie.nu', category='cpu_seconds')
+        fws_genie_nu = [fwm.make(j, 'Genie', 'genie.nu', category='cpu')
                         for j in pre_hadd_range]
-        fws_genie_rock = [fwm.make(j, 'Genie', 'genie.rock', category='cpu_hours')
+        fws_genie_rock = [fwm.make(j, 'Genie', 'genie.rock', category='cpu_long')
                           for j in pre_hadd_range]
-        fws_edep_nu = [fwm.make(j, 'Edep', 'edep.nu', category='cpu_seconds')
+        fws_edep_nu = [fwm.make(j, 'Edep', 'edep.nu', category='cpu')
                        for j in pre_hadd_range]
-        fws_edep_rock = [fwm.make(j, 'Edep', 'edep.rock', category='cpu_hours')
+        fws_edep_rock = [fwm.make(j, 'Edep', 'edep.rock', category='cpu_long')
                          for j in pre_hadd_range]
-        fw_hadd_nu = fwm.make(i, 'Hadd', 'edep.nu.hadd', category='cpu_seconds')
-        fw_hadd_rock = fwm.make(i, 'Hadd', 'edep.rock.hadd', category='cpu_seconds')
-        fw_spill = fwm.make(i, 'SpillBuild', 'spill', category='cpu_seconds')
-        fw_convert2h5 = fwm.make(i, 'Convert2H5', 'convert2h5', category='cpu_seconds')
-        fw_larnd = fwm.make(i, 'LArND', 'larnd', category='gpu_minutes')
-        fw_flow = fwm.make(i, 'Flow', 'flow', category='cpu_minutes')
-        fw_plots = fwm.make(i, 'Plots', 'plots', category='cpu_minutes')
-
-        fw_flow2supera = fwm.make(i, 'Flow2Supera', 'flow2supera', category='cpu_minutes')
-        fw_spine = fwm.make(i, 'Spine', 'spine', category='gpu_minutes')
-        fw_flow2root = fwm.make(i, 'Flow2root', 'flow2root', category='gpu_minutes')
-        fw_pandora = fwm.make(i, 'Pandora', 'pandora', category='cpu_minutes')
-        fw_cafmaker = fwm.make(i, 'CAFmaker','caf', category='cpu_minutes')
-
-        fireworks = [fw_larnd, fw_flow, fw_plots,
-                     fw_flow2supera, fw_spine, fw_cafmaker]
+        fw_hadd_nu = fwm.make(i, 'Hadd', 'edep.nu.hadd', category='cpu')
+        fw_hadd_rock = fwm.make(i, 'Hadd', 'edep.rock.hadd', category='cpu')
+        fw_spill = fwm.make(i, 'SpillBuild', 'spill', category='cpu')
+        fw_edep2flat = fwm.make(i, 'Edep2Flat', 'edep2flat', category='cpu_highmem')
+        fw_minerva = fwm.make(i, 'Minerva', 'minerva', category='cpu')
+        fw_convert2h5 = fwm.make(i, 'Convert2H5', 'convert2h5', category='cpu')
+        fw_larnd = fwm.make(i, 'LArND', 'larnd', category='gpu_long')
+        fw_flow = fwm.make(i, 'Flow', 'flow', category='cpu_highmem')
+        fw_plots = fwm.make(i, 'Plots', 'plots', category='plots')
+        fw_flow2supera = fwm.make(i, 'Flow2Supera', 'flow2supera', category='cpu')
+        fw_spine = fwm.make(i, 'Spine', 'spine', category='gpu')
+        fw_flow2root = fwm.make(i, 'Flow2root', 'flow2root', category='cpu_highmem')
+        fw_pandora = fwm.make(i, 'Pandora', 'pandora', category='cpu')
+        fw_cafmaker = fwm.make(i, 'CAFmaker','caf', category='cpu')
 
         fireworks = [*fws_genie_nu, *fws_genie_rock,
                      *fws_edep_nu, *fws_edep_rock,
-                     fw_hadd_nu, fw_hadd_rock, fw_spill, fw_convert2h5,
-                     fw_larnd, fw_flow, fw_plots,
+                     fw_hadd_nu, fw_hadd_rock, fw_spill,
+                     fw_edep2flat, fw_minerva,
+                     fw_convert2h5, fw_larnd, fw_flow, fw_plots,
                      fw_flow2supera, fw_spine,
                      fw_flow2root, fw_pandora,
                      fw_cafmaker]
@@ -69,8 +68,10 @@ def main():
                 **{fw_edep_rock: [fw_hadd_rock] for fw_edep_rock in fws_edep_rock},
                 fw_hadd_nu: [fw_spill],
                 fw_hadd_rock: [fw_spill],
-                fw_spill: [fw_convert2h5],
+                fw_spill: [fw_convert2h5, fw_edep2flat],
                 fw_convert2h5: [fw_larnd],
+                fw_edep2flat: [fw_minerva],
+                fw_minerva: [fw_cafmaker],
                 fw_larnd: [fw_flow],
                 fw_flow: [fw_flow2supera, fw_flow2root, fw_plots],
                 fw_flow2supera: [fw_spine],
