8e65502e26fc07136bd4db9e6eeb1659638545fc
Matt Kramer <mjkramer@users.noreply.github.com>
2025-03-25 19:51:10 -0700

    Update 2x2 reflow example

--- examples/submit.Reflow_2x2.sh
+++ examples/submit.Reflow_2x2.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-scripts/load_yaml.py specs/Reflow_v1.yaml specs/Reflow_2x2/*.yaml
+source admin/load_fireworks.sh
 
-workflows/fwsub.reflow.py -b Reflow_2x2.flow -i /pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2/ndlar_reflow/inputs.2x2_beam.json
+name=Reflow_2x2
+inputs=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2/ndlar_reflow/inputs.2x2_beam.json
+
+scripts/load_yaml.py specs/Reflow_v1.yaml specs/$name/*.yaml
+
+workflows/fwsub.reflow.py -b $name.flow -i $inputs
+
+logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/slurm_logs/$name
+
+mkdir -p "$logdir"
+
+sbatch -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 128 slurm/fw_cpu.slurm.sh $name.flow rapidfire
