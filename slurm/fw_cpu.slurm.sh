66f184b23973b3436468e981192101bbdcd1a99b
alexbooth92 <a.c.booth92@gmail.com>
2025-05-06 04:53:55 -0700

    Change account to m3249 and tweak to log naming in slurm cpu/gpu scripts.

--- slurm/fw_cpu.slurm.sh
+++ slurm/fw_cpu.slurm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --account=dune
+#SBATCH --account=m3249
 #SBATCH --qos=regular
 #SBATCH --constraint=cpu
 #SBATCH --time=1:00:00
@@ -10,7 +10,7 @@ rlaunch_args=("$@"); shift $#
 
 basedir=$PWD
 
-logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/slurm_logs/${category}/${SLURM_JOBID}
+logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/logs_slurm/${category}/${SLURM_JOBID}
 mkdir -p "$logdir"
 
 launchdir=${FW4DUNE_SCRATCH:-$SCRATCH}/launchers/${category}/${SLURM_JOBID}
--- slurm/fw_gpu.slurm.sh
+++ slurm/fw_gpu.slurm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --account=dune
+#SBATCH --account=m3249
 #SBATCH --qos=regular
 #SBATCH --constraint=gpu
 #SBATCH --time=1:00:00
@@ -12,7 +12,7 @@ rlaunch_args=("$@"); shift $#
 
 basedir=$PWD
 
-logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/slurm_logs/${category}/${SLURM_JOBID}
+logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/logs_slurm/${category}/${SLURM_JOBID}
 mkdir -p "$logdir"
 
 launchdir=${FW4DUNE_SCRATCH:-$SCRATCH}/launchers/${category}/${SLURM_JOBID}
