993492f569827eb95335eb5065247e9ef2e5b181
Matt Kramer <mjkramer@users.noreply.github.com>
2025-11-18 07:58:18 -0800

    More specs for 2x2 Run2

---
+++ examples/submit.Reflow_2x2_Run2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source admin/load_fireworks.sh
+
+name=Reflow_2x2_Run2_v0
+inputs1=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2_Run2_v0/inputs.60116.json # AmBe
+inputs2=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2_Run2_v0/inputs.60130.json # cosmics
+
+scripts/load_yaml.py specs/Reflow_v1.yaml specs/Reflow_2x2_Run2/*.yaml
+
+workflows/fwsub.reflow_plus_downstream.py -i $inputs -n Reflow_2x2_Run2
+
+logdir=${FW4DUNE_SCRATCH:-$SCRATCH}/slurm_logs/$name
+
+mkdir -p "$logdir"
+
+# TODO: Update job parameters
+sbatch -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 192 slurm/fw_cpu.slurm.sh Reflow_2x2.flow rapidfire
+sbatch -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 128 slurm/fw_cpu.slurm.sh Reflow_2x2.flow2supera rapidfire
+sbatch --array=1-2 -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 4 slurm/fw_gpu.slurm.sh Reflow_2x2.spine rapidfire
+sbatch --array=1-3 -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 20 slurm/fw_cpu.slurm.sh Reflow_2x2.flow2root rapidfire
+sbatch -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 128 slurm/fw_cpu.slurm.sh Reflow_2x2.pandora rapidfire
+sbatch -o "$logdir"/slurm-%j.txt -N 4 -t 240 --ntasks-per-node 128 slurm/fw_cpu.slurm.sh Reflow_2x2.caf rapidfire
