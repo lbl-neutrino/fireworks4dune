9e4a6be7123328bc26b3c682fa7bab54310fe19f
Matt Kramer <mjkramer@users.noreply.github.com>
2025-12-07 12:02:25 -0800

    fixup

--- examples/submit.SPINE_train_ND.sh
+++ examples/submit.SPINE_train_ND.sh
@@ -7,6 +7,9 @@ name=SPINE_train_ND_2512
 scripts/load_yaml.py specs/SPINETrainForNDLAr_v1.yaml specs/$name/*.yaml
 workflows/fwsub.SPINE_train_ND.py --size 1
 
+mkdir -p $SCRATCH/mkramer/output/$name
+mkdir -p $SCRATCH/mkramer/logs/$name
+
 # Spread out the worker startups so they don't all connect to Mongo DB at once
 export FW4DUNE_SLEEP_SEC=60
 
