10e936fc8e8005e98ca2694cc72406bdd587598a
Matt Kramer <mjkramer@users.noreply.github.com>
2026-03-08 20:43:10 -0700

    Update Shield_Plane_2601

--- specs/Shield_Plane_2601/generate.sh
+++ specs/Shield_Plane_2601/generate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for d in mpvmpr_gigacube mpvmpr_ndlar nu_ndlar; do
-  pushd $d
-  for f in *.jinja2; do minijinja-cli $f > $(basename $f .jinja2); done
-  popd
+
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+
+for d in mpvmpr_gigacube mpvmpr_ndlar nu_ndlar mpvmpr_2x2; do
+  pushd $d || exit
+  for f in *.jinja2; do minijinja-cli "$f" > "$(basename "$f" .jinja2)"; done
+  popd || exit
 done
