8220c51cb4f0de6eab211efaa0f118faece18e7f
Matt Kramer <mjkramer@users.noreply.github.com>
2026-05-16 18:00:25 -0700

    More FSDCube

--- examples/submit.Reflow_FSDCube.sh
+++ examples/submit.Reflow_FSDCube.sh
@@ -9,3 +9,8 @@ scripts/reset_db.py
 scripts/load_yaml.py specs/Reflow_v4.yaml specs/Reflow_FSDCube/ALL.Reflow_FSDCube.yaml
 
 workflows/fwsub.reflow_centralized.py --name "$name" --inputs-json "$inputs"
+
+
+# TMP for testing
+
+workflows/fwsub.FSDCubeSim_spine.sh
