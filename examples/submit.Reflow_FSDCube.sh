db232b1d34802abd284689c2786a1e46837f4fec
Matt Kramer <mjkramer@users.noreply.github.com>
2026-05-16 17:34:20 -0700

    Update FSDCube reflow

--- examples/submit.Reflow_FSDCube.sh
+++ examples/submit.Reflow_FSDCube.sh
@@ -6,6 +6,6 @@ name=Reflow_FSDCube_v3
 inputs=/pscratch/sd/d/dunepro/mkramer/install/Reflow_FSDCube_v1/files.json
 
 scripts/reset_db.py
-scripts/load_yaml.py specs/Reflow_v3.yaml specs/Reflow_FSDCube/ALL.Reflow_FSDCube.yaml
+scripts/load_yaml.py specs/Reflow_v4.yaml specs/Reflow_FSDCube/ALL.Reflow_FSDCube.yaml
 
 workflows/fwsub.reflow_centralized.py --name "$name" --inputs-json "$inputs"
