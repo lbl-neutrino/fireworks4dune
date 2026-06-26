d98603ea2060699d2cdb33dd46e1ccea6bb7e78a
Jacob Larkin <jacob.larkin@stonybrook.edu>
2026-06-26 09:34:23 -0500

    Add remaining specs (except pandora) for MiniProdN5p3

--- examples/submit_MiniProdN5_NDComplex.sh
+++ examples/submit_MiniProdN5_NDComplex.sh
@@ -453,8 +453,8 @@ mkdir -p $logdir
 #scripts/fwsub.py --runner NDComplex_v1_Convert2H5 --base-env MiniProdN5p3_NDComplex_FHC.convert2h5.full.sanddrift --size $spill_p3_size --start $start_sanddrift
 #sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-10 -N 1 slurm/fw_cpu.slurm.sh MiniProdN5p3_NDComplex_FHC.convert2h5.full.sanddrift rapidfire
 
-#scripts/fwsub.py --runner NDComplex_v1_TMSReco --base-env MiniProdN5p3_NDComplex_RHC.tmsreco.full.sanddrift --size $spill_p3_size --start $start_sanddrift
-#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN5p3_NDComplex_RHC.tmsreco.full.sanddrift rapidfire
+#scripts/fwsub.py --runner NDComplex_v1_TMSReco --base-env MiniProdN5p3_NDComplex_FHC.tmsreco.full.sanddrift --size $spill_p3_size --start $start_sanddrift
+#sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 1 slurm/fw_cpu.slurm.sh MiniProdN5p3_NDComplex_FHC.tmsreco.full.sanddrift rapidfire
 
 #scripts/fwsub.py --runner NDComplex_v1_LArND --base-env MiniProdN5p3_NDComplex_FHC.larnd.full.sanddrift --size $spill_p3_size --start $start_sanddrift
 #sbatch -o ${logdir:-.}/slurm-%j.txt --array=1-1 -N 64 slurm/fw_gpu.slurm.sh MiniProdN5p3_NDComplex_FHC.larnd.full.sanddrift rapidfire
